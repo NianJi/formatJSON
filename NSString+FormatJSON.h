94abd0514b825061c82440a4635b44fd3393f0c2
liukun <765409243@qq.com>
2013-02-18 21:10:55 +0800

    commit code

---
+++ NSString+FormatJSON.h
@@ -0,0 +1,15 @@
+//
+//  NSString+FormatJSON.h
+//  SuningEBuy
+//
+//  Created by  liukun on 13-2-18.
+//  Copyright (c) 2013年 BlueBox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (FormatJSON)
+
+- (NSString *)formatJSON;
+
+@end
---
+++ NSString+FormatJSON.m
@@ -0,0 +1,99 @@
+//
+//  NSString+FormatJSON.m
+//  SuningEBuy
+//
+//  Created by  liukun on 13-2-18.
+//  Copyright (c) 2013年 BlueBox. All rights reserved.
+//
+
+#import "NSString+FormatJSON.h"
+
+@implementation NSString (FormatJSON)
+
+- (NSString *)formatJSON
+{
+    int indentLevel = 0;
+    BOOL inString    = NO;
+    char currentChar = '\0';
+    char *tab = "    ";
+    
+    NSUInteger len = [self lengthOfBytesUsingEncoding:NSUTF8StringEncoding];
+    const char *utf8 = [self UTF8String];
+    NSMutableData *buf = [NSMutableData dataWithCapacity:(NSUInteger)(len * 1.1f)];
+    
+    for (int i = 0; i < len; i++)
+    {
+        currentChar = utf8[i];
+        
+        switch (currentChar) {
+            case '{':
+            case '[':
+                if (!inString) {
+                    [buf appendBytes:&currentChar length:1];
+                    [buf appendBytes:"\n" length:1];
+                    
+                    for (int j = 0; j < indentLevel+1; j++) {
+                        [buf appendBytes:tab length:strlen(tab)];
+                    }
+                    
+                    indentLevel += 1;
+                } else {
+                    [buf appendBytes:&currentChar length:1];
+                }
+                break;
+            case '}':
+            case ']':
+                if (!inString) {
+                    indentLevel -= 1;
+                    [buf appendBytes:"\n" length:1];
+                    for (int j = 0; j < indentLevel; j++) {
+                        [buf appendBytes:tab length:strlen(tab)];
+                    }
+                    [buf appendBytes:&currentChar length:1];
+                } else {
+                    [buf appendBytes:&currentChar length:1];
+                }
+                break;
+            case ',':
+                if (!inString) {
+                    [buf appendBytes:",\n" length:2];
+                    for (int j = 0; j < indentLevel; j++) {
+                        [buf appendBytes:tab length:strlen(tab)];
+                    }
+                } else {
+                    [buf appendBytes:&currentChar length:1];
+                }
+                break;
+            case ':':
+                if (!inString) {
+                    [buf appendBytes:":" length:1];
+                } else {
+                    [buf appendBytes:&currentChar length:1];
+                }
+                break;
+            case ' ':
+            case '\n':
+            case '\t':
+                if (inString) {
+                    [buf appendBytes:&currentChar length:1];
+                }
+                break;
+            case '"':
+                
+                if (i > 0 && utf8[i-1] != '\\')
+                {
+                    inString = !inString;
+                }
+                
+                [buf appendBytes:&currentChar length:1];
+                break;
+            default:
+                [buf appendBytes:&currentChar length:1];
+                break;
+        }
+    }
+    
+    return [[NSString alloc] initWithData:buf encoding:NSUTF8StringEncoding];
+}
+
+@end
